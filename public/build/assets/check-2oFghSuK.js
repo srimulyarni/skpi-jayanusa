dd6d9cc6fd689cc11e8a9ac86db5018fad7dc4cb
Pramtoxz <pramuditometra@gmail.com>
2026-07-19 19:26:00 +0700

    build: rebuild assets frontend

---
+++ public/build/assets/check-2oFghSuK.js
@@ -0,0 +1,1 @@
+import{L as e}from"./app-Cj2Pg5QV.js";var t=e(`Check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
---
+++ public/build/assets/plus-CwZgf7sZ.js
@@ -0,0 +1,1 @@
+import{L as e}from"./app-Cj2Pg5QV.js";var t=e(`Plus`,[[`path`,{d:`M5 12h14`,key:`1ays0h`}],[`path`,{d:`M12 5v14`,key:`s699le`}]]);export{t};
