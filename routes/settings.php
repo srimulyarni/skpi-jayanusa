5e30a1dabb6e4057e7c1c3f58ed4813294630890
Pramtoxz <pramuditometra@gmail.com>
2026-07-20 12:12:00 +0700

    fix: no dark mode

--- routes/settings.php
+++ routes/settings.php
@@ -1,14 +1,10 @@
 <?php
 
-use App\Http\Controllers\Settings\ProfileController;
 use App\Http\Controllers\Settings\SecurityController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', '/settings/profile');
-
-    Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::redirect('settings', '/settings/security');
 
     Route::get('settings/security', [SecurityController::class, 'edit'])->name('security.edit');
     Route::put('settings/password', [SecurityController::class, 'update'])
